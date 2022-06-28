597014b83706340401b9fecbbd1f751cd032bb9f
Pushpak Bhagat <pbhagat@montran.com>
2022-06-28 18:32:37 +0530

    :package: Life in weeks challenge in js

--- JavaScript/basicPrograms/NumbersBasic.js
+++ JavaScript/basicPrograms/NumbersBasic.js
@@ -1,15 +1,25 @@
 /**
  * convert Dogs Age to equivalent Humman Age
  */
-function dogAgeToHumanAge(dogAge)
-{
- return (dogAge-2)*4 + 21;
+function dogAgeToHumanAge(dogAge) {
+    return (dogAge - 2) * 4 + 21;
 }
 
 /** max amout of milk-bottles for Money given */
-function maxMilkBottles(money){
+function maxMilkBottles(money) {
     const price = 1.4;
-    return Math.floor(money/price);
+    return Math.floor(money / price);
 }
 /* */
 
+/**
+ * Life remmaning 
+ * @param {*} age in years
+ */
+function lifeInWeeks(age) {
+    var yearsLeft = 90 - age;
+    var x = yearsLeft * 365;//days
+    var y = yearsLeft * 52; //weeks
+    var z = yearsLeft * 12;// months
+    console.log("You have " + x + " days, " + y + " weeks, and " + z + " months left.");
+}
