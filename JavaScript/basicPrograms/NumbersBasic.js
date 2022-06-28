e37f703dad85c04de31b85a00338f9d800373924
Pushpak Bhagat <pbhagat@montran.com>
2022-06-28 18:17:47 +0530

    :package: Parameterized Fuction in js

--- JavaScript/basicPrograms/NumbersBasic.js
+++ JavaScript/basicPrograms/NumbersBasic.js
@@ -6,3 +6,10 @@ function dogAgeToHumanAge(dogAge)
  return (dogAge-2)*4 + 21;
 }
 
+/** max amout of milk-bottles for Money given */
+function maxMilkBottles(money){
+    const price = 1.4;
+    return Math.floor(money/price);
+}
+/* */
+
